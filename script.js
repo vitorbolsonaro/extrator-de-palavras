834be5e325e8f6e248a31231ad276bba6e6cc62f
vitorbolsonaro <vitor.gustavo.marins@escola.pr.gov.br>
2025-09-16 16:58:30 -0300

    aula dia 16

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ const palavras = texto.split(" ")
 
 campoResultado.textContent = palavras.join("'");
 }
+
+function processaTexto(texto) {
+    let palavra = texto.splint(/\P{L}+/u);
+const frequencias = contafrequencias(palavra)
+let ordenadas = Object.keys(frequencias).sort(ordenapalavra);
+function ordenaPalavra(p1,p2){
+return frequencias [p2]- frequencias[p1];
+
+}
+console.login(ordendas);
+return ordenadas.slice(0,10);
+function contafrequencias(palavras){
+    let frequencias = 0;
+
+    for(let j of palavras){
+        if(j==j){
+            frequencias[i]++;
+        }
+    }
+
+
+}
+
+
+
+return palavras;
+
+
+
+
+}
